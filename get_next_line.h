6433f2723bffb219119c4512a45939fd4435a82f
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-05-09 16:04:51 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 10:59:36 by idumenil          #+#    #+#             */
+/*   Updated: 2023/05/01 11:31:21 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <stddef.h>
 
 #define BUFFER_SIZE 1024
 
