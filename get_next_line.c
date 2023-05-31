5e27769c92b8d54ea3cb2d4bf8b31ca45386569f
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-05-31 13:24:04 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -114,24 +114,3 @@ char	*get_next_line(int fd)
 	stash = ft_next(stash);
 	return (line);
 }
-
-
-#include <fcntl.h>  // open()
-#include <stdio.h>
-int main(void)
-{
-	int fd1;
-	char *line;
-
-	fd1 = open("fichier1", O_RDONLY);
-	line = get_next_line(fd1);
-	while (line)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd1);
-	}
-	close(fd1);
-
-	return (0);
-}
